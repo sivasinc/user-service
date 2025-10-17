ee088887dcd022fe228526fab5b0f307ad8c6df0
sivasinc <prasadsp07@gmail.com>
2025-10-17 06:58:38 -0400

    Add the new file changes

--- src/test/java/com/circles/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/circles/userservice/UserServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.circles.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that application context loads successfully
 	}
-
 }
+
