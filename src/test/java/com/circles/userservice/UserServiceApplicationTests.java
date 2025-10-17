b7be37b4150d76ae10757964f942b3a74001e201
sivasinc <prasadsp07@gmail.com>
2025-10-17 15:33:59 -0400

    added new files

--- src/test/java/com/circles/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/circles/userservice/UserServiceApplicationTests.java
@@ -1,15 +1,23 @@
 package com.circles.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
 	@Test
 	void contextLoads() {
-		// Test that JPA context loads successfully with H2
+		// Verify that JPA context loads successfully
+		assertNotNull(entityManager);
 	}
 }
