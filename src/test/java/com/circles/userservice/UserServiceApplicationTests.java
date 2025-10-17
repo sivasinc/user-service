1b0043e2dfe04ea7d2adda3b75005bf371f328d2
sivasinc <prasadsp07@gmail.com>
2025-10-17 11:59:27 -0400

    added new testfiles

--- src/test/java/com/circles/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/circles/userservice/UserServiceApplicationTests.java
@@ -1,16 +1,18 @@
 package com.circles.userservice;
 
+import com.circles.userservice.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// Test that application context loads successfully
+		// Test that application context loads successfully with H2
 	}
 }
-
